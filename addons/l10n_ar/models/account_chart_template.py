af6abb957d2e7f41898d85d40edab71e99c852e7
Katherine Zaoral <kz@adhoc.com.ar>
2019-08-09 18:28:52 -0300

    [IMP] l10n_ar: manually create demo journals. rempve context in _prepare_all_journals method called when installing COA

--- addons/l10n_ar/models/account_chart_template.py
+++ addons/l10n_ar/models/account_chart_template.py
@@ -18,21 +18,12 @@ class AccountChartTemplate(models.Model):
 
     def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
         """ If Argentinian chart, we don't create sales journal as we need more
-        data to create it properly. We use a extra key in the context in order
-        to properly create the sale journals for demo instances.
-        """
+        data to create it properly """
         res = super()._prepare_all_journals(acc_template_ref, company, journals_dict=journals_dict)
-
         if company.country_id == self.env.ref('base.ar'):
             for vals in res:
                 if vals['type'] == 'sale':
-                    if self._context.get('create_sale_journal'):
-                        vals.update({
-                            'l10n_ar_afip_pos_number': 1, 'l10n_ar_afip_pos_partner_id': company.partner_id.id,
-                            'l10n_ar_afip_pos_system': 'II_IM', 'l10n_ar_share_sequences': True,
-                        })
-                    else:
-                        res.remove(vals)
+                    res.remove(vals)
         return res
 
     @api.model
