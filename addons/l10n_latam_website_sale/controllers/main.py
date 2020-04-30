1348396bb16d01b1206ce3d0fb30ace78eb491cc
Katherine Zaoral <kz@adhoc.com.ar>
2020-04-30 10:31:32 -0300

    portal document type working

--- addons/l10n_latam_website_sale/controllers/main.py
+++ addons/l10n_latam_website_sale/controllers/main.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.http import request, route
+from odoo.exceptions import ValidationError
+from odoo import _
 
 
 class L10nLatamCustomerPortal(CustomerPortal):
@@ -12,18 +13,38 @@ class L10nLatamCustomerPortal(CustomerPortal):
 
     @route()
     def account(self, redirect=None, **post):
-        # identification_type = False
-        # if post and request.httprequest.method == 'POST':
-        #     identification_type = post.pop('l10n_latam_identification_type_id', False)
+        if post and request.httprequest.method == 'POST':
+            post.update({'l10n_latam_identification_type_id': int(post.pop('l10n_latam_identification_type_id') or False) or False})
 
         response = super().account(redirect=redirect, **post)
 
         identification_types = request.env['l10n_latam.identification.type'].sudo().search([])
-        response.qcontext.update({
-            'identification_types': identification_types,
-            })
+        response.qcontext.update({'identification_types': identification_types})
         return response
 
+    def details_form_validate(self, data):
+        error, error_message = super().details_form_validate(data)
+
+        # identification type validation
+        partner = request.env.user.partner_id
+        if data.get("l10n_latam_identification_type_id") and data.get("vat") and partner and \
+           partner.l10n_latam_identification_type_id.id != data.get("l10n_latam_identification_type_id"):
+            if partner.can_edit_vat():
+                if hasattr(partner, "check_vat"):
+                    partner_dummy = partner.new({
+                        'l10n_latam_identification_type_id': data.get('l10n_latam_identification_type_id', False),
+                        'vat': data['vat'], 'country_id': (int(data['country_id']) if data.get('country_id') else False)})
+                    try:
+                        partner_dummy.check_vat()
+                    except ValidationError as exception:
+                        error["vat"] = 'error'
+                        error_message.append(exception.name)
+            else:
+                error_message.append(_('Changing VAT number is not allowed once document(s) have been issued for your account. Please contact us directly for this operation.'))
+
+        return error, error_message
+
+# from odoo.addons.website_sale.controllers.main import WebsiteSale
 # class L10nLatamWebsiteSale(WebsiteSale):
 
 #     def _get_mandatory_billing_fields(self):
