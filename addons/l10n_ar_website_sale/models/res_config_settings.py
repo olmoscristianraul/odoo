f379312dfa2241a29528ef8560ee9e1cb3c34d2a
Katherine Zaoral <kz@adhoc.com.ar>
2020-05-19 19:53:14 -0300

    working by now. need more integrated logic

--- addons/l10n_ar_website_sale/models/res_config_settings.py
+++ addons/l10n_ar_website_sale/models/res_config_settings.py
@@ -13,24 +13,10 @@ class ResConfigSettings(models.TransientModel):
         required=True, default='responsibility_type',
         config_parameter='l10n_ar_website_sale.show_line_subtotals_tax_selection')
 
-    def write(self, vals):
+    def set_values(self):
         """ When changing setting we also update all the portal users """
-        res = super().write(vals)
-        ar_tax_selection = vals.get('l10n_ar_show_line_subtotals_tax_selection')
-        company = self.env.company_id
-        if ar_tax_selection and company.country_id == self.env.ref('base.ar'):
-            users = self.env['res.users'].search([]).filtered(lambda x: x.is_ar_portal_user())
-            print('---- users %s' % users.mapped('name'))
-            if users:
-                if ar_tax_selection == 'tax_excluded':
-                    self.group_show_line_subtotals_tax_included = False
-                    self.group_show_line_subtotals_tax_excluded = True
-                elif ar_tax_selection == 'tax_included':
-                    self.group_show_line_subtotals_tax_included = True
-                    self.group_show_line_subtotals_tax_excluded = False
-                elif ar_tax_selection == 'responsibility_type':
-                    for user in users:
-                        final_consumer = user.l10n_ar_afip_responsibility_type_id == self.env.ref('l10n_ar.res_CF')
-                        self.group_show_line_subtotals_tax_included = True if final_consumer else False
-                        self.group_show_line_subtotals_tax_excluded = False if final_consumer else True
+        res = super().set_values()
+        # TODO compare between the saved and the ir.config.parameter?
+        if self.l10n_ar_show_line_subtotals_tax_selection:
+            self.env['res.users'].search([])._l10n_ar_update_tax_group_portal_user()
         return res
--- addons/l10n_ar_website_sale/models/res_users.py
+++ addons/l10n_ar_website_sale/models/res_users.py
@@ -1,4 +1,4 @@
-from odoo import api, models
+from odoo import models
 from odoo.addons.base.models.res_users import name_selection_groups
 
 
@@ -13,22 +13,28 @@ class ResUsers(models.Model):
             return True
         return False
 
-    # def write(self, values):
-    #     res = super().write(values)
-    #     import pdb; pdb.set_trace()
+    def is_user_type(self):
+        internal = self.env.ref('base.group_user')
+        portal = self.env.ref('base.group_portal')
+        public = self.env.ref('base.group_public')
+        return name_selection_groups([internal.id, portal.id, public.id])
 
-    #     internal = self.env.ref('base.group_user')
-    #     portal = self.env.ref('base.group_portal')
-    #     public = self.env.ref('base.group_public')
-    #     user_type = name_selection_groups([internal.id, portal.id, public.id])
+    def write(self, values):
+        res = super().write(values)
+        if self.is_user_type() in values:
+            self._l10n_ar_update_tax_group_portal_user()
+        return res
 
-    #     # and values.get('user_type') == portal.id
-    #     if user_type in values:
+    # TODO test
+    # def create(self, values):
+    #     res = super().create(values)
+    #     if self.is_user_type() in values:
     #         self._l10n_ar_update_tax_group_portal_user()
     #     return res
 
     def _l10n_ar_update_tax_group_portal_user(self):
-        import pdb; pdb.set_trace()
+        # TODO we need default for non portal users
+        # B2B is the default for new users (portal and internal)
 
         tax_included = self.env.ref('account.group_show_line_subtotals_tax_included')
         tax_excluded = self.env.ref('account.group_show_line_subtotals_tax_excluded')
@@ -37,31 +43,11 @@ class ResUsers(models.Model):
 
         portal_users = self.filtered(lambda x: x.is_ar_portal_user())
         for user in portal_users:
-            print(' --- before %s' % user.groups_id)
-
-            if company_tax_config == 'responsibility_type':
-                final_consumer = user.l10n_ar_afip_responsibility_type_id == self.env.ref('l10n_ar.res_CF')
-                new_values = [(4 if final_consumer else 3, tax_included.id),
-                              (3 if final_consumer else 4, tax_excluded.id)]
+            final_consumer = user.l10n_ar_afip_responsibility_type_id == self.env.ref('l10n_ar.res_CF')
+            if company_tax_config == 'tax_included' or (company_tax_config == 'responsibility_type' and final_consumer):
+                tax_excluded.users -= user
+                tax_included.users |= user
             else:
-                new_values = [(4 if company_tax_config == 'tax_included' else 3, tax_included.id),
-                              (4 if company_tax_config == 'tax_excluded' else 3, tax_excluded.id)]
-
-            user.write({'groups_id': [(3, tax_included.id), (3, tax_excluded.id)]})
-            print(' --- after %s' % user.groups_id)
-            user.write({'groups_id': sorted(new_values)})
-            print(' --- last %s' % user.groups_id)
-        # we need default for non portal user
-
-
-class ResGroups(models.Model):
-
-    _inherit = 'res.groups'
-
-    def write(self, values):
-        res = super().write(values)
-        import pdb; pdb.set_trace()
-        # sel_groups_1_8_9: 8
-        # if 'groups_id' in values:
-        #     self._l10n_ar_update_tax_group_portal_user()
-        return res
+                # company_tax_config == 'tax_excluded' or (company_tax_config == 'responsibility_type' and not final_consumer)
+                tax_included.users -= user
+                tax_excluded.users |= user
