d13249a8e16aa43b075534334bad3b492c6b6f26
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-18 16:57:26 -0300

    [IMP] l10n_ar: argentinian custom invoice report
    * custom reports are only printed when the invoice are not pre printed type
    * update demo data company's email shorter to make it work with the custom layout
    * update translations

--- addons/l10n_ar/models/account_move.py
+++ addons/l10n_ar/models/account_move.py
@@ -186,3 +186,13 @@ class AccountMove(models.Model):
                 'l10n_ar_afip_service_end': move.l10n_ar_afip_service_end,
             })
         return super()._reverse_moves(default_values_list=default_values_list, cancel=cancel)
+
+    def get_custom_report(self, report_xml_id):
+        self.ensure_one()
+        if self.l10n_latam_use_documents and self.company_id.country_id.code == 'AR':
+            custom_report = {
+                'account.report_invoice_document_with_payments': 'l10n_ar.report_invoice_document_with_payments',
+                'account.report_invoice_document': 'l10n_ar.report_invoice_document',
+            }
+            return custom_report.get(report_xml_id) or report_xml_id
+        return super().get_custom_report(report_xml_id)
